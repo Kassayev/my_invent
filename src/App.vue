6b99d041b4993782aa2c8d3f86aba314db6f75dd
Dauren051 <dauren323245@gmail.com>
2023-06-04 18:22:32 +0600

    image changed

--- src/App.vue
+++ src/App.vue
@@ -1508,7 +1508,7 @@ body {
 
 .blok-1 {
 	/* background-image: url(./assets/images/fon-default.png); */
-	background-image: url(./assets/images/1685877153.png);
+	background-image: url(./assets/images/1685881245.png);
 	background-position: center 0;
 	background-repeat: no-repeat;
 	background-size: 100%;
