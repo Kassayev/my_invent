b967376d9c1aaec7db21d71ddae715075d1d8aff
Kassayev <kasaev.serzhan@yandex.ru>
2024-04-25 21:09:49 +0500

    fixes

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -17,7 +17,7 @@
   </template>
   
   <script>
-  import audioFile from "@/assets/audio/Қазыбек Құрайыш - Махаббат деген.mp3";
+  import audioFile from "@/assets/audio/audio.mp3";
   export default {
     data() {
       return {
